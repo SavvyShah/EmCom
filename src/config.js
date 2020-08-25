b6dfa598297e19c301b201dfc2316013d137ab87
Shubham <shubhamshahrising@gmail.com>
2020-08-25 12:57:10 +0530

    Setup environment variables

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export default {
+  apiHost: 'http://localhost:3000' || process.env.SERVER_URL || '',
+  mode: 'development', // can be development or production
+  firebaseConfig: {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || '',
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || '',
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL || '',
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || '',
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || '',
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGER_ID || '',
+    appId: process.env.REACT_APP_FIREBASE_APP_ID || '',
+  },
+}
